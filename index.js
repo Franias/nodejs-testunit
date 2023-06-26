ff19f0a80d8244f5b81d29d8b3475e25b1c9e7b0
Francielli Pinheiro Dias <francielli_dias@terceiros.sicredi.com.br>
2023-06-26 17:44:25 -0300

    adiciona testes carrinho e itens

---
+++ index.js
@@ -0,0 +1,9 @@
+const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
+
+const calculaDesconto = (salario, descontos) => salario - descontos;
+
+export {
+    somaHorasExtras,
+    calculaDesconto,
+};
+
